e43bf9a794408c5d4fc8b352d649ed8a94e1d89e
Lucas Avila <lucaxx@gmail.com>
2022-06-11 03:40:30 -0300

    add markdoc

--- docs/content/select.md
+++ docs/content/select.md
@@ -1,6 +1,86 @@
-# Select
+# Examples
+
+## Table definition
+
+First, create the table definitions.
 
 ```ts
-const t1 = table(["a", "b", "c"], "t1");
-t1.select((f) => ({ a: f.a })).print();
+const users = table(["id", "age", "name"], "users");
 ```
+
+## Select
+
+### Select star
+
+{% printer %}
+
+```ts
+users.selectStar().print();
+```
+
+{% /printer %}
+
+### Select a field
+
+{% printer %}
+
+```ts
+users.select((f) => ({ name: f.name })).print();
+```
+
+{% /printer %}
+
+### Select star and a field
+
+{% printer %}
+
+```ts
+users
+    .selectStar()
+    .appendSelect((f) => ({ otherAlias: f.name }))
+    .print();
+```
+
+{% /printer %}
+
+### Select a field and star
+
+{% printer %}
+
+```ts
+users
+    .select((f) => ({ otherAlias: f.name }))
+    .appendSelectStar()
+    .print();
+```
+
+{% /printer %}
+
+### Select from sub-select
+
+{% printer %}
+
+```ts
+users
+    .selectStar()
+    .select((f) => ({ age: f.age }))
+    .print();
+```
+
+{% /printer %}
+
+### Kitchen sink
+
+{% printer %}
+
+```ts
+const plus50Users = users.selectStar().where((f) => sql`${f.age} > 50`);
+
+plus50Users
+    .select((f) => ({ name: f.name }))
+    .distinct()
+    .limit(2)
+    .print();
+```
+
+{% /printer %}
--- docs/src/app.tsx
+++ docs/src/app.tsx
@@ -1,12 +1,39 @@
-import React, { useEffect } from "react";
-import { table as _table } from "../../src";
-
+import React from "react";
+import { table as _table, sql as _sql } from "../../src";
+import Markdoc, { Tag } from "@markdoc/markdoc";
 import useSWR from "swr";
 
 const table = _table;
-console.error(table);
+const sql = _sql;
+const users = table(["id", "age", "name"], "users");
+
+const printer = {
+    render: "pre",
+    children: ["pre"],
+    attributes: {},
+    transform(node, config) {
+        const attributes = node.transformAttributes(config);
+        const children = node.transformChildren(config);
+
+        const text = children
+            .map((it) => it.children)
+            .reduce((p, c) => [...p, ...c], [])
+            .join("");
+        console.warn({ text, attributes, children, node });
+        return [
+            //
+            children,
+            new Tag(`pre`, { ...attributes }, [eval(text)]),
+        ];
+    },
+};
+
 export function App() {
-    const contentURL = new URL("../content/q.md", import.meta.url).toString();
+    const contentURL = new URL(
+        "../content/select.md",
+        import.meta.url
+    ).toString();
+
     const { data } = useSWR(contentURL, (str) =>
         fetch(str).then((it) => it.text())
     );
@@ -15,11 +42,14 @@ export function App() {
         return <></>;
     }
 
-    return (
-        <>
-            <pre>{data}</pre>
-            <br />
-            <pre>{eval(data)}</pre>
-        </>
-    );
+    const ast = Markdoc.parse(data);
+    const content = Markdoc.transform(ast, {
+        nodes: {},
+        tags: { printer },
+        variables: {},
+    });
+    const rendered = Markdoc.renderers.react(content, React, {
+        components: {},
+    });
+    return <div className="markdown-body">{rendered}</div>;
 }
