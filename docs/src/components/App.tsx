a113f6c3976242007c13edce7b522e6be6b0da09
Lucas Avila <lucaxx@gmail.com>
2022-06-11 17:48:13 -0300

    add top nav

--- docs/src/components/App.tsx
+++ docs/src/components/App.tsx
@@ -5,9 +5,24 @@ import "prismjs/components/prism-typescript";
 import "prismjs/components/prism-sql";
 import { Document } from "./Document";
 import { URLS } from "../lib/urls";
+import { Grommet } from "grommet";
+import { CollapsableNav } from "./TopNav";
 
 export const App: FC = () => (
-  <Suspense fallback={<></>}>
-    <Document contentUrl={URLS.where} />
-  </Suspense>
+  <Grommet
+    theme={{
+      global: {
+        font: {
+          family: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+          size: "18px",
+          height: "24px",
+        },
+      },
+    }}
+  >
+    <CollapsableNav />
+    <Suspense fallback={<></>}>
+      <Document contentUrl={URLS.where} />
+    </Suspense>
+  </Grommet>
 );
