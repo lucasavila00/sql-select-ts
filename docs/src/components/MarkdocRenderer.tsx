c7018c4714e3bc93d9e28a5a32838a8cb8b7d3fc
Lucas Avila <lucaxx@gmail.com>
2022-06-12 01:37:15 -0300

    scroll into view if hash is linked

--- docs/src/components/MarkdocRenderer.tsx
+++ docs/src/components/MarkdocRenderer.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import Markdoc, { RenderableTreeNode } from "@markdoc/markdoc";
 import { Fence } from "./Fence";
 import { Heading } from "grommet";
@@ -17,9 +17,29 @@ const HeadingGrommet: FC<{
   </Heading>
 );
 
+const elementIdFromHash = (): string | null => {
+  const hash = window.location.hash;
+  if (hash == null) {
+    return null;
+  }
+  return hash.replace("#", "");
+};
+
 export const MarkdocRenderer: FC<{
   content: RenderableTreeNode;
 }> = ({ content }) => {
+  useEffect(() => {
+    const elementId = elementIdFromHash();
+    if (elementId == null) {
+      return;
+    }
+    const e = document.getElementById(elementId);
+    if (e == null) {
+      return;
+    }
+    e.scrollIntoView();
+  }, []);
+
   const rendered = Markdoc.renderers.react(content, React, {
     components: { Fence, HeadingGrommet },
   });
