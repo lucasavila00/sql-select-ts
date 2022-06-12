702b842761b8f006668fe89fe4d884ad2e6d2e1c
Lucas Avila <lucaxx@gmail.com>
2022-06-12 01:25:56 -0300

    improve anchor behaviour

--- docs/src/components/AnchorLink.tsx
+++ docs/src/components/AnchorLink.tsx
@@ -1,5 +1,5 @@
 import { Anchor, AnchorExtendedProps } from "grommet";
-import React, { FC, startTransition } from "react";
+import React, { FC, startTransition, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 type AnchorLinkType = FC<
@@ -9,14 +9,18 @@ type AnchorLinkType = FC<
 >;
 export const AnchorLink: AnchorLinkType = ({ to, ...rest }) => {
   const navigate = useNavigate();
+  // force Anchor to re-mount on click, so that it loses the pressed status
+  const [k, setK] = useState(0);
   return (
     <Anchor
       href={to}
+      key={k}
       onClick={(ev) => {
+        ev.preventDefault();
         startTransition(() => {
           navigate(to);
+          setK((it) => it + 1);
         });
-        ev.preventDefault();
       }}
       {...rest}
     />
